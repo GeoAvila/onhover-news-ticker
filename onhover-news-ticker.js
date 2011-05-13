ab3840099092cb488ff238298b56505192e65276
Geovanny G. Avila @GeoAvila <geovanny.avila@gmail.com>
2011-05-13 10:59:38 -0600

    Version 0.1

--- onhover-news-ticker.js
+++ onhover-news-ticker.js
@@ -24,41 +24,37 @@
         var $container=$(this);
         var $text_animate=$(this).find('span').html();
         $(this).find('span').remove();
-       
-            $container.css('width',settings.width+'px');   $container.css('overflow','hidden');
-            $container.css('height',settings.height+'px');
-       
-        
-         $container.append('<div id="text_container_s"><span>'+$text_animate +' | </span><span>'+$text_animate +' | </span></div>');
-         var $text_container=$('#text_container_s');
+        $container.css('width',settings.width+'px');   $container.css('overflow','hidden');
+        $container.css('height',settings.height+'px');
+        $container.append('<div id="text_container_s"><span>'+$text_animate +' | </span><span>'+$text_animate +' | </span></div>');
+        var $text_container=$('#text_container_s');
         $text_container.css('width','1000px');
         var $text_width=$('#text_container_s span').width();
         var $cont_anim=$('#text_container_s span:first');
         var $move_param = $text_width-settings.width;
         var $stop = false; 
- $(" #text_container_s").mouseover(function() {
-  $stop=true; 
-  movement(); 
- });
-  $(" #text_container_s").mouseleave(function() {
-$stop=false;
- }); 
+		$(" #text_container_s").mouseover(function() {
+			$stop=true; 
+			movement(); 
+		});
+		$(" #text_container_s").mouseleave(function() {
+			$stop=false;
+		}); 
         function movement(){
             var $cont_anim=  $('#text_container_s span:first') ;
           
-          $cont_anim.animate({
-            marginLeft: '-='+($text_width)+'',
-          }, settings.speed, function() {
-           $('#text_container_s').append('<span>'+$text_animate +' | </span>');
-         $('#text_container_s span:first').remove();
-           if($stop!=false)
-           {   
-          movement();
-           }
-          });
+			$cont_anim.animate({
+				marginLeft: '-='+($text_width)+'',
+				}, settings.speed, function() {
+				$('#text_container_s').append('<span>'+$text_animate +' | </span>');
+				$('#text_container_s span:first').remove();
+			if($stop!=false)
+				{   
+					movement();
+				}
+			});
     
         };
-        
          function gostop(object){
             var $cont_anim=$(object).find('span').first();
           $cont_anim.animate({
@@ -66,7 +62,6 @@ $stop=false;
           }, settings.speed);
           this.stop();
         }; 
-
     });
 
   };
