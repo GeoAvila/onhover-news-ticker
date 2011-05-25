51ed180253698164c3a17b827fa3d69ed5626e77
Geovanny G. Avila @geoavila <geovanny.avila@gmail.com>
2011-05-25 09:30:16 -0600

    Version 0.3

--- onhover-news-ticker.js
+++ onhover-news-ticker.js
@@ -11,7 +11,9 @@
     var settings = {
       'width'         : 100,
       'height'         : 25,
-      'speed' : 5000
+      'speed' : 5000,
+	  'padding':15,
+	  'on_hover' : false
     };
 
     return this.each(function() {        
@@ -21,7 +23,9 @@
         $.extend( settings, options );
       }
         // textanimate plugin code
+		$(this).find('span').css('padding-right',settings.padding+'px')
         var $text_animate=$(this).html();
+		
         $(this).html('<div class="text_container"></div>');
         $(this).find('.text_container').append($text_animate+$text_animate);
         var $container=$(this);
@@ -32,19 +36,38 @@
         var $text_width=$text_container.find('span').width();
         var $cont_anim=$text_container.find('span').first();
         var $move_param = $text_width-settings.width;
-        var $stop = false; 
         $text_container.mouseover(function() {
-            $stop=true; 
-            movement(); 
+		    if(settings.on_hover==true)
+			{
+				$stop=true; 
+				movement(); 				
+			}else
+			{
+			$text_container.find('span').first().stop();
+			}
         });
         $text_container.mouseleave(function() {
-            $stop=false;
-        }); 
+		    if(settings.on_hover==true)
+			{
+			    $stop=false;
+			}else
+			{
+				$stop=true; 
+				movement(); 
+			}
+        });
+		var $stop = settings.on_hover; 
+		if(settings.on_hover!=true)
+		{
+		$stop=true;
+		 movement();
+		}
+		
         function movement(){
             var $cont_anim=  $text_container.find('span').first();
             $cont_anim.animate({
-                marginLeft: '-='+($text_width)+'',
-                }, settings.speed, function() {
+                marginLeft: '-='+($text_width+settings.padding)+''
+                },settings.speed,"linear", function() {
                 $text_container.append($text_animate);
                 $text_container.find('span').first().remove();
             if($stop!=false)
